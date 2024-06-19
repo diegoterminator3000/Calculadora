3ddebd6f97483f3244096e7e799f34c594cc55f1
diegoterminator3000 <di.penaylillo@duocuc.cl>
2024-06-19 15:40:57 -0400

    Update python.py

--- python.py
+++ python.py
@@ -2,5 +2,7 @@ num1 = int(input("Ingrese num1"));
 num2 = int(input("Ingrese num2"));
 
 multiplicacion = num1*num2
-print(multiplicacion)
+print(multiplicacion) 
+
+print("Perú nos va a volar la raja")
 
